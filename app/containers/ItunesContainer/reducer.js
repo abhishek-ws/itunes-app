e6519860f0fb40bb261159c50ca8b73f8afa9f29
Abhishek Mishra <abhishek@wednesday.is>
2021-09-10 17:25:01 +0530

    Updated tests for Reducer, Saga and Selectors, now having TrackDetails Page State

--- app/containers/ItunesContainer/reducer.js
+++ app/containers/ItunesContainer/reducer.js
@@ -10,15 +10,24 @@ import { translate } from '@app/components/IntlGlobalProvider/';
 
 export const initialState = {
   searchTerm: null,
+  trackId: null,
   gridData: {},
-  searchError: null
+  trackDetails: {},
+  songsCache: {},
+  searchError: null,
+  trackSearchError: null
 };
 
 export const { Types: itunesContainerTypes, Creators: itunesContainerCreators } = createActions({
   searchItunes: ['searchTerm'],
   successSearchItunes: ['data'],
   failureSearchItunes: ['error'],
-  clearGridData: {}
+  searchTrack: ['trackId'],
+  successSearchTrack: ['data'],
+  failureSearchTrack: ['error'],
+  trackDetails: {},
+  clearGridData: {},
+  clearTrackDetails: {}
 });
 
 /* eslint-disable default-case, no-param-reassign */
@@ -40,6 +49,24 @@ export const itunesContainerReducer = (state = initialState, action) =>
       case itunesContainerTypes.CLEAR_GRID_DATA:
         draft.searchTerm = null;
         draft.gridData = {};
+        break;
+      case itunesContainerTypes.SEARCH_TRACK:
+        draft.trackSearchError = null;
+        draft.trackDetails = {};
+        draft.trackId = action.trackId;
+        break;
+      case itunesContainerTypes.SUCCESS_SEARCH_TRACK:
+        draft.trackDetails = {};
+        draft.trackSearchError = null;
+        draft.songsCache[draft.trackId] = action.data;
+        draft.trackDetails = draft.songsCache[draft.trackId].results[0];
+        break;
+      case itunesContainerTypes.FAILURE_SEARCH_TRACK:
+        draft.trackDetails = {};
+        draft.trackSearchError = get(action.error, 'message', translate('something_went_wrong'));
+        break;
+      case itunesContainerTypes.CLEAR_TRACK_DETAILS:
+        draft.trackDetails = {};
         break;
     }
   });
--- app/containers/ItunesContainer/saga.js
+++ app/containers/ItunesContainer/saga.js
@@ -1,9 +1,13 @@
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
 import { itunesContainerTypes, itunesContainerCreators } from './reducer';
-import { getSongs } from '@services/songsApi';
+import { getSongs, getTrackDetails } from '@services/songsApi';
+import { selectSongsCache } from './selectors';
+import { isEmpty } from 'lodash';
 
-const { SEARCH_ITUNES } = itunesContainerTypes;
-const { successSearchItunes, failureSearchItunes } = itunesContainerCreators;
+const { SEARCH_ITUNES, SEARCH_TRACK } = itunesContainerTypes;
+const { successSearchItunes, failureSearchItunes, successSearchTrack, failureSearchTrack } = itunesContainerCreators;
+
+// const songsCache = selectSongsCache();
 
 export function* requestSearchItunes(action) {
   const res = yield call(getSongs, action.searchTerm);
@@ -15,6 +19,28 @@ export function* requestSearchItunes(action) {
   }
 }
 
+export function* requestTrackDetails(action) {
+  let songsCache;
+  if (!isEmpty(action.testSagaCache)) {
+    songsCache = action.testSagaCache;
+  } else {
+    songsCache = yield select(selectSongsCache());
+  }
+  if (!isEmpty(songsCache) && songsCache[action.trackId]) {
+    const data = songsCache[action.trackId];
+    yield put(successSearchTrack(data));
+  } else {
+    const res = yield call(getTrackDetails, action.trackId);
+    const { data, ok } = res;
+    if (ok) {
+      yield put(successSearchTrack(data));
+    } else {
+      yield put(failureSearchTrack(data));
+    }
+  }
+}
+
 export default function* itunesContainerSaga() {
   yield takeLatest(SEARCH_ITUNES, requestSearchItunes);
+  yield takeLatest(SEARCH_TRACK, requestTrackDetails);
 }
