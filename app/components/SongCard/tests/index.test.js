77fa590732841d880088bedea407db3bcdc0491d
Abhishek Mishra <abhishek@wednesday.is>
2021-09-10 17:22:40 +0530

    Updated tests for SongCard

--- app/components/SongCard/tests/index.test.js
+++ app/components/SongCard/tests/index.test.js
@@ -8,9 +8,26 @@ import React from 'react';
 import { fireEvent } from '@testing-library/dom';
 import { renderWithIntl } from '@utils/testUtils';
 import SongCard from '../index';
+import ReturnWithRouter from '@utils/returnRouter';
+import { setIntl, translate } from '@app/components/IntlGlobalProvider/';
+import getIntl from '@utils/createIntl';
+
+function SongWrapper(props) {
+  return (
+    <ReturnWithRouter>
+      <SongCard {...props} />
+    </ReturnWithRouter>
+  );
+}
 
 describe('<SongCard/> tests', () => {
+  beforeAll(() => {
+    setIntl(getIntl());
+  });
   const song = {
+    trackName: 'Track',
+    trackPrice: 200,
+    trackId: 1234,
     shortDescription: 'Song Description',
     artworkUrl100:
       'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
@@ -19,24 +36,84 @@ describe('<SongCard/> tests', () => {
   };
 
   it('should render and match the snapshot', () => {
-    const { baseElement } = renderWithIntl(<SongCard song={song} />);
+    const { baseElement } = renderWithIntl(<SongWrapper song={song} />);
     expect(baseElement).toMatchSnapshot();
   });
 
   it('should contain 1 SongCard component', () => {
-    const { getAllByTestId } = renderWithIntl(<SongCard song={song} />);
+    const { getAllByTestId } = renderWithIntl(<SongWrapper song={song} />);
     expect(getAllByTestId('song-card').length).toBe(1);
   });
 
   it('should play the song when "Play Button" clicked', () => {
-    const { getByTestId } = renderWithIntl(<SongCard song={song} />);
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
     fireEvent.click(getByTestId('play-btn'));
     expect(getByTestId('audio-element').src).toBe(song.previewUrl);
   });
 
   it('should stop the song when "Stop Button" clicked', () => {
-    const { getByTestId } = renderWithIntl(<SongCard song={song} />);
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
     fireEvent.click(getByTestId('stop-btn'));
     expect(getByTestId('audio-element').src).toBe('');
+  });
+
+  it('should render No-price translation when trackPrice is not available', () => {
+    const song = {
+      trackName: 'Track',
+      trackPrice: '',
+      trackId: 1234,
+      shortDescription: 'Song Description',
+      artworkUrl100:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
+      artistName: 'Artist Name',
+      previewUrl: 'https://mockurl.com/'
+    };
+
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
+    expect(getByTestId('no-price-tag')).toHaveTextContent(translate('no-price'));
+  });
+
+  it('should render the song`s short description accordingly when rendering the card on Home Page / Grid from given from song prop', () => {
+    const song = {
+      trackName: 'Track',
+      trackPrice: 200,
+      trackId: 1234,
+      shortDescription: 'SongShortDescription',
+      artworkUrl100:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
+      artistName: 'Artist Name',
+      previewUrl: 'https://mockurl.com/'
+    };
+
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
+    expect(getByTestId('para-test')).toHaveTextContent(song.shortDescription);
+  });
+
+  it('should render the song`s long description accordingly when rendering the card on Track Details Page from given from song prop', () => {
+    const song = {
+      trackName: 'Track',
+      trackPrice: 200,
+      trackId: 1234,
+      shortDescription: 'SongShortDescription',
+      artworkUrl100:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
+      artistName: 'Artist Name',
+      previewUrl: 'https://mockurl.com/'
+    };
+
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
+    expect(getByTestId('para-test')).toHaveTextContent(song.shortDescription);
+  });
+
+  it('should disable the Play button once, song is played', () => {
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
+    fireEvent.click(getByTestId('play-btn'));
+    expect(getByTestId('play-btn')).toBeDisabled();
+  });
+
+  it('should disable the Stop button once, song is stopped', () => {
+    const { getByTestId } = renderWithIntl(<SongWrapper song={song} />);
+    fireEvent.click(getByTestId('stop-btn'));
+    expect(getByTestId('stop-btn')).toBeDisabled();
   });
 });
