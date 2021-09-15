13fa93fc3aa644878a12de581d7473373cb1d69e
Abhishek Mishra <abhishek@wednesday.is>
2021-09-15 13:04:00 +0530

    Minor fix SongCard

--- app/components/SongCard/index.js
+++ app/components/SongCard/index.js
@@ -10,7 +10,7 @@ import styled from 'styled-components';
 import { Card, Typography, Button } from 'antd';
 import { PlayCircleTwoTone, StopTwoTone } from '@ant-design/icons';
 import { colors } from '@app/themes';
-import { translate } from '@components/IntlGlobalProvider/';
+import { translate } from '@components/IntlGlobalProvider';
 import { Link } from 'react-router-dom';
 import If from '@components/If';
 import { T } from '@components/T';
