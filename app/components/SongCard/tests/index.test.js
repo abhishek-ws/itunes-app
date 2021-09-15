be4e54fb9c35eafa9e1168019d5933ca2f93cac0
Abhishek Mishra <abhishek@wednesday.is>
2021-09-15 12:47:21 +0530

    Updated test strategy for SongCard Component

--- app/components/SongCard/tests/index.test.js
+++ app/components/SongCard/tests/index.test.js
@@ -15,16 +15,20 @@ describe('<SongCard/> tests', () => {
   beforeAll(() => {
     setIntl(getIntl());
   });
-  const song = {
-    trackName: 'Track',
-    trackPrice: 200,
-    trackId: 1234,
-    shortDescription: 'Song Description',
-    artworkUrl100:
-      'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
-    artistName: 'Artist Name',
-    previewUrl: 'https://mockurl.com/'
-  };
+
+  let song;
+  beforeEach(() => {
+    song = {
+      trackName: 'Track',
+      trackPrice: 200,
+      trackId: 1234,
+      shortDescription: 'Song Description',
+      artworkUrl100:
+        'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdribbble.com%2Fshots%2F14395014-Music-Logo&psig=AOvVaw3I_T0J8_ZRKW_g4VF_KmKz&ust=1630933127025000&source=images&cd=vfe&ved=2ahUKEwiT5uO-8efyAhWVHLcAHVSbCvQQjRx6BAgAEAk',
+      artistName: 'Artist Name',
+      previewUrl: 'https://mockurl.com/'
+    };
+  });
 
   it('should render and match the snapshot', () => {
     const { baseElement } = renderWithIntl(<SongCard song={song} />);
@@ -37,7 +41,7 @@ describe('<SongCard/> tests', () => {
   });
 
   it('should render No-price translation when trackPrice is not available', () => {
-    const song = {
+    song = {
       trackName: 'Track',
       trackPrice: null,
       trackId: 1234,
@@ -53,7 +57,7 @@ describe('<SongCard/> tests', () => {
   });
 
   it('should render the song`s short description accordingly when rendering the card on Home Page / Grid from given from song prop', () => {
-    const song = {
+    song = {
       trackName: 'Track',
       trackPrice: 200,
       trackId: 1234,
@@ -69,7 +73,7 @@ describe('<SongCard/> tests', () => {
   });
 
   it('should render the song`s long description accordingly when rendering the card on Track Details Page from given from song prop', () => {
-    const song = {
+    song = {
       trackName: 'Track',
       trackPrice: 200,
       trackId: 1234,
