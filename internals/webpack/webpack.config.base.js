f4d7f094e028cacfa30c92391a2c92b44b21f9ea
Abhishek Mishra <abhishek@wednesday.is>
2021-09-20 12:11:52 +0530

    uat ready to deploy

--- internals/webpack/webpack.config.base.js
+++ internals/webpack/webpack.config.base.js
@@ -5,8 +5,10 @@ const path = require('path');
 const webpack = require('webpack');
 const dotenv = require('dotenv');
 const colors = require('../../app/themes/colors');
+const { getBasePublicPath } = require('../utils');
+const publicPath = getBasePublicPath();
 
-const dotEnvFile = process.env.ENVIRONMENT_NAME === 'production' ? `.env` : `.env.${process.env.ENVIRONMENT_NAME}`;
+const dotEnvFile = process.env.NODE_ENV === 'production' ? `.env` : `.env.${process.env.NODE_ENV}`;
 const env = dotenv.config({ path: dotEnvFile }).parsed;
 const envKeys = {
   ...Object.keys(process.env).reduce((prev, next) => {
@@ -27,7 +29,7 @@ module.exports = (options) => ({
       // Compile into js/build.js
       path: path.resolve(process.cwd(), 'build'),
       // to run prod build locally, set public path to '/' instead of '/react-template/'
-      publicPath: '/'
+      publicPath
     },
     options.output
   ), // Merge with env dependent settings
